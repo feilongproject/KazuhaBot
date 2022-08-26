c08f602baeb91d965c9cfb9cf852d8db3fe8eac6
feilongproject <flzs6137@gmail.com>
2022-08-26 21:51:54 +0800

    perf: 优化指令json，更新匹配逻辑

--- src/plugins/system/findOpts.ts
+++ src/plugins/system/findOpts.ts
@@ -4,14 +4,25 @@ import log from "./logger";
 
 export async function findOpts(optStr: string): Promise<string> {
 
+    const command: {
+        [mainKey: string]: {
+            [key: string]: {
+                reg: string;
+                fnc: string;
+                describe: string;
+            }
+        }
+    } = fnc.command;
 
-    for (let i = 0; i < fnc.length; i++) {
-        const opt = fnc[i];
-        //log.debug(optStr, RegExp(opt.reg), optStr.match(RegExp(opt.reg)));
-        if (RegExp(opt.reg).test(optStr)) {
-            return opt.fnc;
-        };
+    for (const mainKey in command) {
+        for (const key in command[mainKey]) {
+            const opt = command[mainKey][key];
+            if (RegExp(opt.reg).test(optStr)) {
+                return opt.fnc;
+            };
+        }
     }
+
 
     return "err";
 }
