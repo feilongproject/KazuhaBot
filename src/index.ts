93a4ada021dd48c9a4fb36c450c5020811088439
feilongproject <flzs6137@gmail.com>
2022-09-11 04:12:29 +0800

    feat: 为频道内消息添加findOpts

--- src/index.ts
+++ src/index.ts
@@ -9,40 +9,28 @@ var checkTimes = 0;
 
 init().then(() => {
 
-    global.ws.on('GUILD_MESSAGES', (data: IntentMessage) => {
+    global.ws.on('GUILD_MESSAGES', async (data: IntentMessage) => {
         const msg = new IMessageEx(data.msg, "GUILD");// = data.msg as any;
         global.redis.set("lastestMsgId", msg.id, { EX: 5 * 60 });
 
         if (data.eventType == "MESSAGE_CREATE") {
             const opts = msg.content.trim().split(" ");
-            /* findOpts(opts[0]).then(opt => {
-                switch (opt) {
-                    case "ping":
-                        global.redis.ping().then(m => {
-                            msg.sendMsgEx({ content: m });
-
-                        }).catch(err => {
+            const opt = await findOpts(opts[0]);
+            log.debug(`./plugins/${opt.path}:${opt.fnc}`);
+            try {
+                if (opt.path != "err") {
+                    const plugin = await import(`./plugins/${opt.path}.ts`);
+                    if (typeof plugin[opt.fnc] == "function") {
+                        (plugin[opt.fnc] as PluginFnc)(msg).catch(err => {
                             log.error(err);
                         });
-                        break;
-                    case "gacha":
-                        gacha(msg);
-                        break;
-                    case "onceDaily":
-                        onceCheck(msg);
-                        break;
-                    case "changeDaily":
-                        break;
-                    case "helpDaily":
-                        break;
-                    case "modeDaily":
-                        break;
-                    default:
-                        break;
+                    } else {
+                        log.error(`not found function ${opt.fnc}() at "${global._path}/src/plugins/${opt.path}.ts"`);
+                    }
                 }
-
-            }); */
-
+            } catch (err) {
+                log.error(err);
+            }
         }
     });
 
