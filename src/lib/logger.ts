ca1117f62498749926d677505941a78977533713
feilongproject <flzs6137@gmail.com>
2022-09-14 09:50:52 +0800

    chore: 更改log等级为all

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -12,7 +12,7 @@ const log = log4js.configure({
     categories: {
         default: {
             appenders: ["console"],
-            level: "info",
+            level: "all",
             enableCallStack: true,
         }
     },
