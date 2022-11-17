394e87dac53f600038cda1bf43c5a3f9a90db095
feilongproject <flzs6137@gmail.com>
2022-11-17 16:23:37 +0800

    perf: 优化主函数处理逻辑，增加对以`/`开始的指令的适配

--- src/index.ts
+++ src/index.ts
@@ -5,56 +5,34 @@ import { IMessageEx } from './lib/IMessageEx';
 init().then(() => {
 
     global.ws.on('GUILD_MESSAGES', async (data: IntentMessage) => {
+        if (data.eventType != "MESSAGE_CREATE") return;
+        const msg = new IMessageEx(data.msg, "GUILD");
+        execute(msg);
 
-        if (data.eventType == "MESSAGE_CREATE") {
-            const msg = new IMessageEx(data.msg, "GUILD");// = data.msg as any;
-            global.redis.set("lastestMsgId", msg.id, { EX: 5 * 60 });
-
-            const opt = await findOpts(msg);
-            log.debug(`./plugins/${opt.path}:${opt.fnc}`);
-            try {
-                if (opt.path != "err") {
-                    const plugin = await import(`./plugins/${opt.path}.ts`);
-                    if (typeof plugin[opt.fnc] == "function") {
-                        (plugin[opt.fnc] as PluginFnc)(msg).catch(err => {
-                            log.error(err);
-                        });
-                    } else {
-                        log.error(`not found function ${opt.fnc}() at "${global._path}/src/plugins/${opt.path}.ts"`);
-                    }
-                }
-            } catch (err) {
-                log.error(err);
-            }
-        }
     });
 
     global.ws.on("DIRECT_MESSAGE", async (data: IntentMessage) => {
         if (data.eventType != 'DIRECT_MESSAGE_CREATE') return;
-        const msg = new IMessageEx(data.msg, "DIRECT");// = data.msg as any;
-        global.redis.set("lastestMsgId", msg.id, { EX: 5 * 60 });
+        const msg = new IMessageEx(data.msg, "DIRECT");
         global.redis.hSet(`genshin:config:${msg.author.id}`, "guildId", msg.guild_id);
-
-        const opt = await findOpts(msg);
-        log.debug(`./plugins/${opt.path}:${opt.fnc}`);
-
-        try {
-            if (opt.path != "err") {
-                const plugin = await import(`./plugins/${opt.path}.ts`);
-                if (typeof plugin[opt.fnc] == "function") {
-                    (plugin[opt.fnc] as PluginFnc)(msg).catch(err => {
-                        log.error(err);
-                    });
-                } else {
-                    log.error(`not found function ${opt.fnc}() at "${global._path}/src/plugins/${opt.path}.ts"`);
-                }
-            }
-        } catch (err) {
-            log.error(err);
-        }
-
+        execute(msg);
     });
 
 });
+
+async function execute(msg: IMessageEx) {
+    global.redis.set("lastestMsgId", msg.id, { EX: 4 * 60 });
+    msg.content = msg.content.trim().replace(/^\//, "#");
+    const opt = await findOpts(msg);
+    if (opt.path != "err") {
+        log.debug(`./plugins/${opt.path}:${opt.fnc}`);
+        const plugin = await import(`./plugins/${opt.path}.ts`);
+        if (typeof plugin[opt.fnc] == "function") {
+            return (plugin[opt.fnc] as PluginFnc)(msg).catch(err => {
+                log.error(err);
+            });
+        } else log.error(`not found function ${opt.fnc}() at "${global._path}/src/plugins/${opt.path}.ts"`);
+    }
+}
 
 type PluginFnc = (msg: IMessageEx) => Promise<any>
